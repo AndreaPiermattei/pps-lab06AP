d46b89895650eb4cc4b2ac6d2c605ff21e96245b
Andrea Piermattei - andrea.piermattei@studio.unibo.it <andrea.piermattei@studio.unibo.it>
2026-04-13 10:32:05 +0200

    lenght metod v1

--- src/main/scala/it/unibo/pps/ex1/OOFPList.scala
+++ src/main/scala/it/unibo/pps/ex1/OOFPList.scala
@@ -46,8 +46,12 @@ enum List[A]:
   
   // Exercise: implement the following methods
   def zipWithValue[B](value: B): List[(A, B)] = ???
-  def length(): Int = ???
-  def indices(): List[A] = ???
+  def length(): Int =
+    var counterElements = 0
+    this.foreach(element => counterElements=counterElements+1)
+    counterElements
+
+  def indices(): List[Int] = ???
   def zipWithIndex: List[(A, Int)] = ???
   def partition(predicate: A => Boolean): (List[A], List[A]) = ???
   def span(predicate: A => Boolean): (List[A], List[A]) = ???
@@ -67,8 +71,8 @@ object List:
 object Test extends App:
   import List.*
   val reference = List(1, 2, 3, 4)
-  println(reference.zipWithValue(10)) // List((1, 10), (2, 10), (3, 10), (4, 10))
   println(reference.length()) // 4
+  println(reference.zipWithValue(10)) // List((1, 10), (2, 10), (3, 10), (4, 10))
   println(reference.indices()) // List(0, 1, 2, 3)
   println(reference.zipWithIndex) // List((1, 0), (2, 1), (3, 2), (4, 3))
   println(reference.partition(_ % 2 == 0)) // (List(2, 4), List(1, 3))
